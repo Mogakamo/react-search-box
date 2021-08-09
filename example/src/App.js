26bfb6d1d9064090e5dacb9fdaf5a6a35cda8532
Mogakamo <mogaka.amo254@gmail.com>
2021-08-09 10:54:23 +0300

    implemented the ReactSearchBox class and it is ok

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-import { ExampleComponent } from 'react-search-box'
+import ReactSearchBox  from 'react-search-box'
 import 'react-search-box/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <ReactSearchBox text="Create React Library Example 😄" />
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+class ReactSearchBox extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
 }
+
+export default ReactSearchBox
