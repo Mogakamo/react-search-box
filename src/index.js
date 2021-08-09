ffc5036d1fec43e451c8a30f9384d6e555e02a5d
Mogakamo <mogaka.amo254@gmail.com>
2021-08-09 11:15:29 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import PropTypes from 'prop-types'
 import styles from './styles.module.css'
 
 class ReactSearchBox extends Component {
+  static PropTypes = {
+    /**
+     * value:
+     */
+  }
+  
+  
+  
   render() {
     return (
       <div>
